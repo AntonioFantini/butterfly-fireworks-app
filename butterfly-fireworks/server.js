178994ee0da58f4ce9f3baf59e41a3f636e0a46a
Antonio Fantini <antonio.fantini88@gmail.com>
2015-11-05 12:29:25 +0100

    commit

--- butterfly-fireworks/server.js
+++ butterfly-fireworks/server.js
@@ -1,43 +1,44 @@
 var express   =    require("express");
 var dao= require('./mysql/dao.js');
+var path    = require("path");
 
 var app       =    express();
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname+ '/public/index.html'));
+});
+
+
 app.get("/dao/getFornitori",function(req,res){
     var callback = function(err, result) {
-    if(err){
-        res.write(
-      '<h1>'+err+'</h1>'
-       );
-    }
-  //  res.writeHead('200', {
-    //  'Content-Type' : 'x-application/json'
-    // });
-    res.write(
-      '<h1>'+JSON.stringify(result)+'</h1>'
-       );
-    console.log('json:', result);
-    res.end();
-};
-	var result = dao.getFornitori(callback);
+        if(err){
+            res.write(
+                err
+           );
+        }
+        res.writeHead('200', {
+            'Content-Type' : 'x-application/json'
+        });
+        res.end(JSON.stringify(result));
+    };
+	dao.getFornitori(callback);
 });
 
 app.get("/dao/getAvailableItemsInStore",function(req,res){
     var callback = function(err, result) {
-    if(err){
-        res.write(
-      '<h1>'+err+'</h1>'
-       );
-    }
-   // res.writeHead('200', {
-     // 'Content-Type' : 'x-application/json'
-    // });
-    res.write(
-      '<h1>'+JSON.stringify(result)+'</h1>'
-       );
-    console.log('json:', result);
-    res.end();
-};
+        if(err){
+            res.write(
+                err
+           );
+        }
+        res.writeHead('200', {
+            'Content-Type' : 'x-application/json'
+        });
+        res.end(JSON.stringify(result));
+    };
 	dao.getAvailableItemsInStore(callback);
 });
 
